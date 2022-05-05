d721450d45bfca1f2fdc61f676eaafc85bb9c0f6
aymanedkhissirevo <Elle3Aymanio>
2022-05-05 21:02:32 +0200

    Initial commit

---
+++ app/src/main/java/fr/aymane/dkhissi/ankarashopping/MainActivity.kt
@@ -0,0 +1,11 @@
+package fr.aymane.dkhissi.ankarashopping
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/fr/aymane/dkhissi/ankarashopping/ui/SplashScreenFragment.kt
@@ -0,0 +1,36 @@
+package fr.aymane.dkhissi.ankarashopping.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import fr.aymane.dkhissi.ankarashopping.R
+import fr.aymane.dkhissi.ankarashopping.databinding.FragmentSplashScreenBinding
+
+
+class SplashScreenFragment : Fragment() {
+
+    private lateinit var binding: FragmentSplashScreenBinding
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentSplashScreenBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.txt.setOnClickListener {
+            findNavController().navigate(R.id.action_splashScreenFragment_to_logSignFragment)
+        }
+    }
+
+
+
+}
